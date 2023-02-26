24643bc8088d82b2fa470f899e49f11a78cca226
iXB3 <matteocausio@yahoo.it>
2023-02-27 00:13:36 +0100

    add BookPromptsCreator

--- metabook/txt.py
+++ metabook/txt.py
@@ -3,6 +3,10 @@ import typing as tp
 from .open_ai import GPT3
 from .models import *
 
+__all__ = (
+
+)
+
 
 class TxtCreator:
     def __init__(self, txt_request: BaseTxtRequest | None = None,
@@ -24,14 +28,13 @@ class TxtCreator:
 
         return created
 
+class TxtAnalysis(pdt.BaseModel):
+    characters: tp.Dict[str, str]
 
 class TxtAnalyzer:
-    def __init__(self, text: str):
-        self.text: str = text
-        self.characters: tp.Dict[str, str] = dict()
-
-    def _characters(self):
-        prompt = f"Text:{self.text}\nList characters visual descriptions:"
+    @classmethod
+    def _characters(cls, text: str) -> tp.Dict[str, str]:
+        prompt = f"Text:{text}\nList characters visual descriptions:"
         raw = GPT3.create(text_in=prompt)
         print(f"RAW CHARACTERS:\n{raw}")
         # TODO: place here replace for each line
@@ -41,7 +44,41 @@ class TxtAnalyzer:
             splitted = l.split(':')
             if len(splitted) == 2:
                 characters[splitted[0]] = splitted[1]
-        self.characters = characters
+        return characters
 
-    def analyze(self):
-        self._characters()
+    @classmethod
+    def analyze(cls, text: str) -> TxtAnalysis:
+        return TxtAnalysis(characters=cls._characters(text=text))
+
+class PagePrompt:
+    def __init__(self, idx: int, txt: str, prompt: str):
+        self.idx: int = int(idx)
+        self.txt: str = str(txt)
+        self.prompt: str = str(prompt)
+
+
+class BookPromptsCreator:
+    def get_current_chars(self, sentence: str, characters: tp.Dict[str, str]) -> tp.List[str]:
+        selected = list()
+        for char in characters.keys():
+            if char in sentence:
+                selected.append(char)
+        return selected
+
+    def create(self, text: str, title: str, style: str) -> tp.List[PagePrompt]:
+        analysis = TxtAnalyzer.analyze(text=text)
+        sentences = tuple(text.replace('\n', '').split('.'))
+        suffix = "Create an illustration description from the text"
+        prompt_list = list()
+        for idx, line in enumerate(sentences):
+            description = GPT3.create(text_in=f"Text:{line}", suffix=suffix, creativity_risk=0.0,
+                                      one_sentence_mode=True)
+            characters = self.get_current_chars(sentence=line, characters=analysis.characters)
+            prompt = ""
+            for char in characters:
+                prompt += f"{char} {analysis.characters[char]}, "
+            prompt = f"{prompt}, {description}, {style} style"
+            prompt_list.append(PagePrompt(idx=idx, txt=line, prompt=prompt))
+        return prompt_list
+
+
